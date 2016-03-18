336f907ecd83f8e1e1388e4d20239adde7747d8a
laithalsunni <laith@alsunni.net>
2016-03-18 22:06:23 +0000

    make it better

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   
   #get 'harwaretrigger/harwaretrigger'
   
-  get '/harwaretrigger', to: 'HardwaretriggerController#hardwaretrigger'
+  get '/harwaretrigger', to: 'hardwaretrigger#hardwaretrigger', as: 'hardwaretrigger'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
