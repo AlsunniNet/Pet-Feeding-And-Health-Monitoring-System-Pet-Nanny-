556e8590decf5dafad7282b74e32d6442f1f3a5c
Jeyabalaji Chandrasekaran <jchandr2@binghamton.edu>
2016-03-24 19:40:34 -0400

    make it better

--- config/routes.rb
+++ config/routes.rb
@@ -31,8 +31,6 @@ Rails.application.routes.draw do
   root :to => 'home#index'
   
   get 'home/index', to: 'home#index', as: 'home'
-
-  
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
